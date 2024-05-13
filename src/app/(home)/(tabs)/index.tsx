f1112835a3453c8c0ebc57e16a093666156a2032
Sovannborith <yun.sovannborith@gmail.com>
2024-05-14 02:40:39 +0700

    implement "connect user avatar to stream chat"

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { useAuth } from "@/src/providers/AuthProvider";
 import { router } from "expo-router";
 import { ChannelList } from "stream-chat-expo";
 
 export default function MainTabScreen() {
+  const { user } = useAuth();
   return (
     <ChannelList
+      filters={{ members: { $in: [user!.id] } }}
       onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
     />
   );
